b6fb07c6116ba3eef0303ce3466d7938ae074d1c
Smart Network <elocal8022@gmail.com>
2024-11-19 19:41:37 +0530

    Update installp.sh

--- installp.sh
+++ installp.sh
@@ -6,3 +6,4 @@ pkg install termux-x11-nightly
 pkg install pulseaudio
 clear
 wget https://raw.githubusercontent.com/LinuxDroidMaster/Termux-Desktops/main/scripts/proot_debian/startcinnamon_debian.sh
+clear
